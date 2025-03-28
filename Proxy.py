1a3f1112ffe9e6866050a61036d7ea59ed2caae3
mjyhi <464791442@qq.com>
2025-03-29 10:03:27 +1030

    Accept connection from client and store socket

--- Proxy.py
+++ Proxy.py
@@ -55,6 +55,7 @@ while True:
   # Accept connection from client and store in the clientSocket
   try:
     # ~~~~ INSERT CODE ~~~~
+    clientSocket, clientAddress = serverSocket.accept()
     # ~~~~ END CODE INSERT ~~~~
     print ('Received a connection')
   except:
