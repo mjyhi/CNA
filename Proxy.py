91bbef639a380928a007b0e5dbc89881d730caf6
mjyhi <464791442@qq.com>
2025-03-29 11:00:26 +1030

    Write origin server response to local cache file

--- Proxy.py
+++ Proxy.py
@@ -188,6 +188,7 @@ while True:
 
       # Save origin server response in the cache file
       # ~~~~ INSERT CODE ~~~~
+      cacheFile.write(originServerResponse)
       # ~~~~ END CODE INSERT ~~~~
       cacheFile.close()
       print ('cache file closed')
