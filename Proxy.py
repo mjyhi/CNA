01bec2f13264ec034696f4ec72395d19ef9a36b2
mjyhi <464791442@qq.com>
2025-03-29 10:34:48 +1030

    Receive HTTP response from origin server

--- Proxy.py
+++ Proxy.py
@@ -171,7 +171,7 @@ while True:
 
       # Get the response from the origin server
       # ~~~~ INSERT CODE ~~~~
-      
+      originServerResponse = originServerSocket.recv(BUFFER_SIZE)
       # ~~~~ END CODE INSERT ~~~~
 
       # Send the response to the client
