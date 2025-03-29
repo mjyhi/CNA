00cb5085262b34079c942df8624543d052941b52
mjyhi <464791442@qq.com>
2025-03-29 10:36:09 +1030

    Send origin server response back to client

--- Proxy.py
+++ Proxy.py
@@ -176,6 +176,7 @@ while True:
 
       # Send the response to the client
       # ~~~~ INSERT CODE ~~~~
+      clientSocket.sendall(originServerResponse)
       # ~~~~ END CODE INSERT ~~~~
 
       # Create a new file in the cache for the requested file.
